dcd66dc042fd05c02afd0b94c3b787bbbbd64859
CursedSpirit64 <70708555+CursedSpirit64@users.noreply.github.com>
2020-09-10 19:40:52 -0400

    Apple moving forward

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
+    public float speed = 40;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class MoveForward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
