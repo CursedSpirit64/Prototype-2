ae886064b4049f57dbfd4d8925f36a6685fac0c9
CursedSpirit64 <70708555+CursedSpirit64@users.noreply.github.com>
2020-09-10 20:55:02 -0400

    ProjectileBoundary

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -17,6 +17,9 @@ public class DestroyOutOfBounds : MonoBehaviour
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
+        } else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
         }
     }
 }
